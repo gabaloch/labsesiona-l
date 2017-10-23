3ddbc4492b7775db6b0baf8183c4891315b4a957
gabaloch <32627458+gabaloch@users.noreply.github.com>
2017-10-23 12:09:23 +0500

    Update Iterator.java

--- shopingcalculator/src/shopingcalculator/Iterator.java
+++ shopingcalculator/src/shopingcalculator/Iterator.java
@@ -1,6 +1,7 @@
 package shopingcalculator;
 
 public interface Iterator {
+	//methods which gives us the check, that whatz next in the cart
 	 public boolean hasNext();
 	   public Object next();
 }
