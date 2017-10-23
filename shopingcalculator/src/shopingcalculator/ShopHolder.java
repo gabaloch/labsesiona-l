07cb3f7e82d772b1f8af524764bdec0c27007fae
gabaloch <32627458+gabaloch@users.noreply.github.com>
2017-10-23 11:51:00 +0500

    Update ShopHolder.java

--- shopingcalculator/src/shopingcalculator/ShopHolder.java
+++ shopingcalculator/src/shopingcalculator/ShopHolder.java
@@ -2,12 +2,16 @@ package shopingcalculator;
 
 public class ShopHolder {
 	public static void main(String[] args) {
+		//from here accessing or repositry
 	      ShopRepository namesRepository = new ShopRepository();
-
+		
+               //from "itrator" interface we are geting th functionilty  overridin method 
+		//also cheking the condition which kept 
 	      for(Iterator iter = namesRepository.getIterator(); iter.hasNext();){
-	    	  
+		      
+	    	  //in this point we r geting the total price  ahich we acording to our formula 
 	         float totalPrice = (float)iter.next();
-	         
+	         //and here we r printing our price
 	         System.out.println("your total  : " + totalPrice);
 	      } 	
 	   }
